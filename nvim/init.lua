3203c63ccfc28819bf2562fb97616d6271903be9
Daniel <danielk@lightbitslabs.com>
2024-03-24 15:48:42 +0200

    added sidebar and lualine

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,6 +29,12 @@ require('pckr').add {
   -- Theme
   'morhetz/gruvbox';
 
+  -- lualine
+  { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } };
+
+  -- SIDEBAR
+  'sidebar-nvim/sidebar.nvim';
+
   -- Git Blame
   'f-person/git-blame.nvim';
   'tpope/vim-fugitive';
@@ -62,7 +68,55 @@ require('pckr').add {
   -- LSP
   'prabirshrestha/vim-lsp';
   'ncm2/ncm2-vim-lsp';
+
 }
+
+-- lualine
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'gruvbox',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
+
+-- SIDEBAR
+local sidebar = require("sidebar-nvim")
+local opts = {open = true}
+sidebar.setup(opts)
 
 -- enable ncm2
 vim.cmd("autocmd BufEnter * call ncm2#enable_for_buffer()")
