dc3d244210f948be3b19fdb9f5350991ee2d2028
danielk-lb <danielk@lightbitslabs.com>
2024-05-12 11:18:54 +0000

    merged build06 changes
    Signed-off-by: danielk-lb <danielk@lightbitslabs.com>

--- nvim/lua/dannightmare/lazy/fugitive.lua
+++ nvim/lua/dannightmare/lazy/fugitive.lua
@@ -1,6 +1,6 @@
 return {
     "tpope/vim-fugitive",
-    config = function() 
+    config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
         local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
@@ -15,14 +15,14 @@ return {
                 end
 
                 local bufnr = vim.api.nvim_get_current_buf()
-                local opts = {buffer = bufnr, remap = false}
+                local opts = { buffer = bufnr, remap = false }
                 vim.keymap.set("n", "<leader>p", function()
                     vim.cmd.Git('push')
                 end, opts)
 
                 -- rebase always
                 vim.keymap.set("n", "<leader>P", function()
-                    vim.cmd.Git({'pull',  '--rebase'})
+                    vim.cmd.Git({ 'pull', '--rebase' })
                 end, opts)
 
                 -- NOTE: It allows me to easily set the branch i am pushing and any tracking
