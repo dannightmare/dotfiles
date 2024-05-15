9ab3cb660e04bcca5d65d3453713fe8db6c55e91
Daniel Kusai <dan.kusai@gmail.com>
2024-05-15 19:10:24 +0300

    Fixed the iron.nvim call
    Signed-off-by: Daniel Kusai <dan.kusai@gmail.com>

--- nvim/lua/dannightmare/lazy/iron.lua
+++ nvim/lua/dannightmare/lazy/iron.lua
@@ -1,45 +1,47 @@
 return {
     'Vigemus/iron.nvim',
     config = function()
-        -- Whether a repl should be discarded or not
-        scratch_repl = true
-        -- Your repl definitions come here
-        repl_definition = {
-            sh = {
-                -- Can be a table or a function that
-                -- returns a table (see below)
-                command = { "zsh" }
-            }
-        }
-        -- How the repl window will be displayed
-        -- See below for more information
-        repl_open_cmd = require("iron.view").split.horizontal.botright(0.3)
+        require('iron.core').setup({
+            config = {
+                scratch_repl = true,
+                repl_open_cmd = require("iron.view").split.horizontal.botright(0.3),
+            },
+            repl_definition = {
+                py = {
+                    command = { "python3" }
+                },
+                sh = {
+                    -- Can be a table or a function that
+                    -- returns a table (see below)
+                    command = { "zsh" }
+                }
+            },
+            keymaps = {
+                send_motion = "<leader>sc",
+                visual_send = "<leader>sc",
+                send_file = "<leader>sf",
+                send_line = "<leader>sl",
+                send_until_cursor = "<leader>su",
+                send_mark = "<leader>sm",
+                mark_motion = "<leader>mc",
+                mark_visual = "<leader>mc",
+                remove_mark = "<leader>md",
+                cr = "<leader>s<cr>",
+                interrupt = "<leader>s<leader>",
+                exit = "<leader>sq",
+                clear = "<leader>cl",
+            },
+            -- If the highlight is on, you can change how it looks
+            -- For the available options, check nvim_set_hl
+            highlight = {
+                italic = true
+            },
+            ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+        })
+
         vim.keymap.set('n', '<leader>rs', '<cmd>IronRepl<cr>')
         vim.keymap.set('n', '<leader>rr', '<cmd>IronRestart<cr>')
         vim.keymap.set('n', '<leader>rf', '<cmd>IronFocus<cr>')
         vim.keymap.set('n', '<leader>rh', '<cmd>IronHide<cr>')
-    end,
-    -- Iron doesn't set keymaps by default anymore.
-    -- You can set them here or manually add keymaps to the functions in iron.core
-    keymaps = {
-        send_motion = "<leader>sc",
-        visual_send = "<leader>sc",
-        send_file = "<leader>sf",
-        send_line = "<leader>sl",
-        send_until_cursor = "<leader>su",
-        send_mark = "<leader>sm",
-        mark_motion = "<leader>mc",
-        mark_visual = "<leader>mc",
-        remove_mark = "<leader>md",
-        cr = "<leader>s<cr>",
-        interrupt = "<leader>s<leader>",
-        exit = "<leader>sq",
-        clear = "<leader>cl",
-    },
-    -- If the highlight is on, you can change how it looks
-    -- For the available options, check nvim_set_hl
-    highlight = {
-        italic = true
-    },
-    ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+    end
 }
