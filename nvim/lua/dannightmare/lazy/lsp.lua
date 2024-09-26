7fb86206fc58106e82568417606e02e3f43dcfaf
Daniel Kusai <danielk@lightbitslabs.com>
2024-09-26 11:56:56 +0000

    added bufls pylsp and removed rust-an

--- nvim/lua/dannightmare/lazy/lsp.lua
+++ nvim/lua/dannightmare/lazy/lsp.lua
@@ -28,8 +28,10 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
-                "rust_analyzer",
+                -- "rust_analyzer",
                 "gopls",
+                "bufls",
+                "pylsp@1.5.0",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
