228af8de1748c83dd747690537281bd2095a77da
dannightmare <dan.kusai.star.123234@gmail.com>
2025-04-05 19:55:30 +0300

    nvim-tree added to replace netrw

---
+++ nvim/lua/dannightmare/lazy/tree.lua
@@ -0,0 +1,28 @@
+return {
+    "nvim-tree/nvim-tree.lua",
+    config = function()
+        -- disable netrw at the very start of your init.lua
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+
+        -- optionally enable 24-bit colour
+        vim.opt.termguicolors = true
+
+        require("nvim-tree").setup({
+            sort = {
+                sorter = "case_sensitive",
+            },
+            view = {
+                width = 30,
+            },
+            renderer = {
+                group_empty = true,
+            },
+            filters = {
+                dotfiles = true,
+            },
+        })
+
+        vim.api.nvim_create_user_command('Explore', require("nvim-tree.api").tree.open, {})
+    end
+}
